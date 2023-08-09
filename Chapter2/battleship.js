b8a408e297b8bacee96b3a0bf544f4c91311a9a2
dvd ahn <dsangahn@gmail.com>
2023-08-09 04:37:43 -0400

    added variable declarations for if locations were hit, alert user

--- Chapter2/battleship.js
+++ Chapter2/battleship.js
@@ -13,6 +13,20 @@ while (isSunk == false) {
     if (guess < 0 || guess > 6) {
         alert ("Please enter a valid cell number!");
     } else {
-        guesses = guesses + 1;
+    guesses = guesses + 1;
+    
+    if (guess == location1 || guess == location2 || guess == location3) {
+        hits = hits + 1;
+
+        if (hits == 3) {
+            
+            isSunk = true;
+            alert ("You sank my battleship!");
+
+            }
+        }
     }
 }
+var percent = (3/guesses * 100);
+var stats = "You took " + guesses + " guesses to sink the battleship, " + "which means your shooting accuracy was " + percent.toFixed(2) + "%";
+alert(stats);
