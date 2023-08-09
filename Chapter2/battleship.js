af9d4e98c2826642cc9b366987a78bba30777e85
dvd ahn <dsangahn@gmail.com>
2023-08-09 05:00:15 -0400

    added alerts if user input was a hit or a miss then user's stats once battleship sunk

--- Chapter2/battleship.js
+++ Chapter2/battleship.js
@@ -14,16 +14,15 @@ while (isSunk == false) {
         alert ("Please enter a valid cell number!");
     } else {
     guesses = guesses + 1;
-    
     if (guess == location1 || guess == location2 || guess == location3) {
+        alert("HIT!");
         hits = hits + 1;
-
-        if (hits == 3) {
-            
+        if (hits == 3) {          
             isSunk = true;
             alert ("You sank my battleship!");
-
             }
+        } else {
+            alert("MISS!");
         }
     }
 }
